ca036363c3000190b98596819b35231bcf4d0233
Siddhartha Das <siddhartha.das.min19@itbhu.ac.in>
2021-09-08 12:46:11 +0530

    GlobalDescriptorTable Part Donegit add .! Such a big pain with jumbled bits can't do it myself without looking up for it seriously

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,6 @@
 
 #include "types.h"
+#include "gdt.h"
 
 void printf(char* str){
     static uint16_t* VideoMemory = (uint16_t*) 0xb8000;
@@ -21,6 +22,8 @@ extern "C" void callConstructors(){
 
 extern "C" void kernelMain(void *multiboot_structure, uint32_t /*multiboot_magic*/){
     printf("Hello, kernel world!");
+
+    GlobalDescriptorTable gdt;
 
     while(1);
 }
